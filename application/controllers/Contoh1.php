38d3a6c6bd4e24c6396346b2d3fa35807e631e83
denspt <cldeni82@gmail.com>
2022-11-26 17:57:46 +0700

    tugas

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Deni Saputra
+            Saya tinggal di tanjung raya 1
+            hobi sepak bola";
+    }
+}
